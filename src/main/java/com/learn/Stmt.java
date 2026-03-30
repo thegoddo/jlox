732ea61b29f1c20517a3d94f595ca2afbe731eb2
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-03-30 14:07:56 +0530

    Add Block and Var statement classes with visitor methods

--- src/main/java/com/learn/Stmt.java
+++ src/main/java/com/learn/Stmt.java
@@ -1,12 +1,25 @@
 package com.learn;
 
+import java.util.List;
+
 abstract class Stmt {
     interface Visitor<R> {
+        R visitBlockStmt(Block stmt);
         R visitExpressionStmt(Expression stmt);
-
         R visitPrintStmt(Print stmt);
+        R visitVarStmt(Var stmt);
     }
+    static class Block extends Stmt {
+        Block(List<Stmt> statements) {
+            this.statements = statements;
+        }
 
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBlockStmt(this);
+        }
+
+        final List<Stmt> statements;
+    }
     static class Expression extends Stmt {
         Expression(Expr expression) {
             this.expression = expression;
@@ -18,7 +31,6 @@ abstract class Stmt {
 
         final Expr expression;
     }
-
     static class Print extends Stmt {
         Print(Expr expression) {
             this.expression = expression;
@@ -29,6 +41,19 @@ abstract class Stmt {
         }
 
         final Expr expression;
+    }
+    static class Var extends Stmt {
+        Var(Token name, Expr initializer) {
+            this.name = name;
+            this.initializer = initializer;
+        }
+
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitVarStmt(this);
+        }
+
+        final Token name;
+        final Expr initializer;
     }
 
     abstract <R> R accept(Visitor<R> visitor);
