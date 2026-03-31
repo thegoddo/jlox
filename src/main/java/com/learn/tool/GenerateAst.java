1a9bdc9d9246ff4f20b370aee20aa8d7f13ed6cf
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-03-31 17:49:04 +0530

    Format AST definitions for improved readability in GenerateAst class

--- src/main/java/com/learn/tool/GenerateAst.java
+++ src/main/java/com/learn/tool/GenerateAst.java
@@ -14,8 +14,9 @@ public class GenerateAst {
         String outputDir = args[0];
 
         defineAst(outputDir, "Expr", Arrays.asList(
-                "Assign : Token name, Expr value",
+                "Assign   : Token name, Expr value",
                 "Binary   : Expr left, Token operator, Expr right",
+                "Call     : Expr callee, Token paren, List<Expr> arguments",
                 "Grouping : Expr expression",
                 "Literal  : Object value",
                 "Logical  : Expr left, Token operator, Expr right",
@@ -23,13 +24,14 @@ public class GenerateAst {
                 "Variable : Token name"));
 
         defineAst(outputDir, "Stmt", Arrays.asList(
-                "Block : List<Stmt> statements",
+                "Block      : List<Stmt> statements",
                 "Expression : Expr expression",
-                "If         : Expr condition, Stmt thenBranch," +
-                        " Stmt elseBranch",
+                "Function   : Token name, List<Token> params, List<Stmt> body",
+                "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
                 "Print      : Expr expression",
-                "Var : Token name, Expr initializer",
-                "While  : Expr condition, Stmt body"));
+                "Return     : Token keyword, Expr value",
+                "Var        : Token name, Expr initializer",
+                "While      : Expr condition, Stmt body"));
     }
 
     private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
