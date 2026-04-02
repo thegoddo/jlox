bbb9d3e40bf35eb38f2ba47629ac97fcb9300a27
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-04-02 12:02:45 +0530

    Update .gitignore to exclude program.lox and configure Maven Jar plugin with main class

--- src/main/java/com/learn/LoxFunction.java
+++ src/main/java/com/learn/LoxFunction.java
@@ -14,7 +14,7 @@ class LoxFunction implements LoxCallable {
     }
 
     LoxFunction bind(LoxInstance instance) {
-        Environment environment = new Environment();
+        Environment environment = new Environment(closure);
         environment.define("this", instance);
         return new LoxFunction(declaration, environment, isInitializer);
     }
